63fed84ca3d7e5e8ebfff19a5fdc5c2d271ac169
yaoius <dilong.yao@gmail.com>
2016-07-18 21:02:03 -0500

    made file executable

--- tree/tree.py
+++ tree/tree.py
@@ -1,9 +1,10 @@
-#!/Users/Min/anaconda3/bin/python
+#!/usr/bin/env python
 
 from argparse import ArgumentParser
 import sys
 import os
 import functools
+import time
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,7 +30,7 @@ def compare(s1, s2):
     else:
         return 0
 
-def trace(directory, tabs, order, list_size, depth, color):
+def trace(directory, tabs, order, show_size, depth, color, show_time):
     items = os.listdir(directory)
     fullpaths = [os.path.join(directory, item) for item in items]
     to_iter = zip(items, fullpaths)
@@ -44,10 +45,12 @@ def trace(directory, tabs, order, list_size, depth, color):
                 else:
                     print('{0}+- {1}'.format(space, item))
                 if depth > 0:
-                    trace(fullpath, tabs + 1, order, list_size, depth - 1, color)
+                    trace(fullpath, tabs + 1, order, show_size, depth - 1, color, show_time)
             else:
-                if list_size:
-                    (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(fullpath)
+                (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(fullpath)
+                if show_time:
+                    item = '[{0}] {1}'.format(time.ctime(mtime), item)
+                if show_size:
                     item = '[{0}] {1}'.format(size, item)
                 print('{0}|-- {1}'.format(space, item))
 
@@ -56,13 +59,14 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(description='Lists files and directories in tree structure')
     parser.add_argument('-o', '--order', dest='order', default=False, action='store_true', help='outputs by files first')
-    parser.add_argument('-s', '--filesize', dest='show_sizes', default=False, action='store_true', help='display file sizes in bytes')
+    parser.add_argument('-s', '--filesize', dest='size', default=False, action='store_true', help='display file sizes in bytes')
     parser.add_argument('-r', '--root', dest='root', type=str, default=os.getcwd(), help='sets root directory')
     parser.add_argument('-d', '--depth', dest='depth', type=int, default=50, help='sets max recursive depth to display')
     parser.add_argument('-c', '--color', dest='color', default=True, action='store_false', help='turns off colored directories')
+    parser.add_argument('-t', '--time', dest='time', default=False, action='store_true', help='shows when file was last modified')
     args = parser.parse_args()
 
     if os.path.isdir(args.root):
-        trace(args.root, 0, args.order, args.show_sizes, args.depth, args.color)
+        trace(args.root, 0, args.order, args.size, args.depth, args.color, args.time)
     else:
         print('{0} is not a valid directory'.format(args.root))
