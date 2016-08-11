46195055bb164085730d34f64bae3dafbc649fad
yaoius <dilong.yao@gmail.com>
2016-08-10 19:20:49 -0500

    basic server operations functional

--- yaoserver/yaoserver.py
+++ yaoserver/yaoserver.py
@@ -1,6 +1,7 @@
 from communications import Request, Response
 from threading import Thread
 from time import gmtime, strftime
+from dbconnect import TableInserter
 import socket
 import sys
 import re
@@ -27,26 +28,36 @@ def init():
     else:
         raise Exception('Invalid Application Root')
 
+    try:
+        global connection_logger, req_logger, res_logger
+        connection_logger = TableInserter()
+        req_logger = TableInserter()
+        res_logger = TableInserter()
+    except Exception as e:
+        print('Warning: MySQL Inserter Failed to instantiate')
+        print(str(e))
+
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     ip = socket.gethostbyname(socket.gethostname())
     sock.bind((socket.gethostname(), port))
     sock.listen(5)
-    print('server started on', '{0}:{1}'.format(ip, port), end='\n\n')
+    print('server started on {0}:{1}\n\n'.format(ip, port))
     connection = 0
 
     while True:
         client_sock, client_addr = sock.accept()
         print(connection, ': Connection from', client_addr)
         connection += 1
-        t = Thread(target=handle_request, args=(client_sock,))
+        t = Thread(target=handle_request, args=(client_sock, client_addr))
         t.start()
 
-def handle_request(client):
+def handle_request(client, addr):
     raw_req = receive(client)
     res = Response()
-    res.add_header('Date', strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+    creation_time = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+    res.add_header('Date', creation_time)
     connection_open = True
 
     try:
@@ -56,8 +67,9 @@ def handle_request(client):
             if req.headers['Connection'] == 'keep-alive':
                 res.add_header('Connection', 'keep-alive')
         match_uri(req.uri, req, res)
+        log(addr, req, res)
     except Exception as e:
-        print(str(e))
+        print(str(e) + '\n')
         res.clear()
         if str(e) == 'Empty Request String':
             connection_open = False
@@ -68,15 +80,13 @@ def handle_request(client):
     if connection_open:
         respond(client, res)
 
-    log(res)
 
 def receive(client):
     raw = client.recv(2048)
     return raw
 
 def respond(client, res):
-    # res.add_header('Date', time)
-    print(res, end='\n\n')
+    print(res)
     client.send(res.encode())
     client.shutdown(1)
     client.close()
@@ -91,13 +101,17 @@ def match_uri(uri, req, res):
         print('No match found')
         res.set_code(404)
 
-def log(res):
-    pass
+def log(addr, req, res):
+    print(addr)
+    print(req)
+    print(res)
 
 
 if __name__ == '__main__':
-    print('yaoserver v0.2.0', end='\n\n')
+    print('yaoserver v0.2.0')
     init()
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
     try:
         import switchboard
     except Exception as e:
