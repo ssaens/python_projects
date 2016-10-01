05f81306cb78b7f6ba3561547fe3f246fff0ce2f
Dillon Yao <dillon@Flicker.local>
2016-10-01 02:20:30 -0700

    safetly copy

--- markovbot/markovchain.py
+++ markovbot/markovchain.py
@@ -1,5 +1,6 @@
-import random
 from sys import argv
+import random
+import argparse
 
 class MarkovChain:
     def __init__(self):
@@ -7,16 +8,34 @@ class MarkovChain:
 
     def add_link(self, val, link):
         if val in self.nodes:
-            self.nodes[val].append(link)
+            self.nodes[val][link] += 1
         else:
-            self.nodes[val] = [link]
+            self.nodes[val] = {link : 1}
 
     def get_rand_start(self):
         return random.choice(list(self.nodes))
 
     def get_next(self, start):
-        return random.choice(self.nodes[start])
+        #return random.choice(self.nodes[start])
+        node_total = sum([freq for link, freq in self.nodes[src]])
+        if node_total = 0:
+            return None
+        selection = random.randint(0, node_total - 1)
+        lower = 0
+        for link, freq in self.nodes[src]:
 
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.out_deg = 0
+        self.links = dict()
+
+    def add_link(self, link):
+        if link in self.links:
+            self.links[link] += 1
+        else:
+            self.links[link] = 1
 
 if __name__ == '__main__':
     if len(argv) > 1:
@@ -35,22 +54,3 @@ if __name__ == '__main__':
         retstr += ' ' + new
         start = (start[1], new)
     print(retstr)
-# class Node:
-#
-#     def __init__(self, val):
-#         self.val = val
-#         self.out_deg = 0
-#         self.links = dict()
-#
-#     def add_link(self, node, freq):
-#         self.links[node].append(node)
-#
-#     def get_neighbor(self):
-#         choice = random.randInt(0, tot_out_freq):
-#         start_range = 0
-#         end_range = 0
-#         for node, freq in self.links:
-#             end_range += freq
-#             if start_range <= choice < end_range:
-#                 return node
-#             start_range = end_range
